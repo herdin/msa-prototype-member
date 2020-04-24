1b6554709a5bb9900dae4e6582f34dbbe0c660c4
Jang <721letteryo@gmail.com>
2020-04-24 16:48:52 +0900

    3 commit

--- src/main/java/com/jsm/MainApp.java
+++ src/main/java/com/jsm/MainApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(MainApp.class);
         SpringApplication.run(MainApp.class);
     }
 }
